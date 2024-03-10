9d7d5e050744bcacfe6f2437fd56b201f913cfde
imorenoca <imorenoca@outlook.com>
2024-03-10 02:44:39 +0100

    Configuración pom.xml con dependenciasMYSQL y conexión a la base de datos

---
+++ src/main/java/jdbc/TestMySqlJDBC.java
@@ -0,0 +1,33 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestMySqlJDBC {
+
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/db_springboot?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			try {
+				Connection conexion = DriverManager.getConnection(url, "root", "admin");
+				Statement instruccion = conexion.createStatement();
+				String sql = "SELECT id, name, lastname, programming_language from persons ";
+				ResultSet resultado = instruccion.executeQuery(sql);
+				while(resultado.next()) {
+					System.out.println("ID: " + resultado.getInt("id"));
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace(System.out);
+		}
+	}
+
+}
